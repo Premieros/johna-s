0bf4a11cf706637bea008d55b43ba24497880c56
Premier <sayed3la2@gmail.com>
2026-09-11 22:03:42 +0300

    feat(desktop): expose thermal print queue diagnostics

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('electronAPI', Object.freeze({
   isElectron: true,
   getPrinters: () => ipcRenderer.invoke('pos:get-printers'),
   printSilent: (options) => ipcRenderer.invoke('pos:print-silent', options),
+  getPrintQueueState: () => ipcRenderer.invoke('pos:get-print-queue-state'),
   kickDrawer: (printerName) => ipcRenderer.invoke('pos:kick-drawer', printerName),
   getSystemInfo: () => ipcRenderer.invoke('pos:get-system-info'),
 }));
