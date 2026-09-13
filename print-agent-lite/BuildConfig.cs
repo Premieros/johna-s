71fd2e5e2bdf6c999df72d40922d5512ce6d04cd
Premier <sayed3la2@gmail.com>
2026-09-13 22:14:50 +0300

    feat(print-lite): add locked Supabase build config

---
+++ print-agent-lite/BuildConfig.cs
@@ -0,0 +1,8 @@
+namespace PremierPrintAgentLite
+{
+    internal static class BuildConfig
+    {
+        internal const string SupabaseUrl = "https://azzdesuowpdcoflmyezn.supabase.co";
+        internal const string AnonKey = "__VITE_SUPABASE_ANON_KEY__";
+    }
+}
