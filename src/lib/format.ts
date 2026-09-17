2293d4db322696f0d05416e1d92bc6a149f26666
Premier <sayed3la2@gmail.com>
2026-09-17 09:41:05 +0300

    fix(security): restore complete HTML escaping

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -161,6 +161,7 @@ export function escapeHtml(value: unknown): string {
   return String(value ?? '')
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
     .replace(/[\x22]/g, '&quot;')
     .replace(/'/g, '&#39;');
 }
