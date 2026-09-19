ce1d406eae1645bf101e103847f837939f0e75ad
Premier <sayed3la2@gmail.com>
2026-09-19 20:35:16 +0300

    chore(print-agent): fix v6 startup message

--- local-print-agent/start.cmd
+++ local-print-agent/start.cmd
@@ -11,7 +11,7 @@ if errorlevel 1 (
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0restart-agent.ps1"
 if errorlevel 1 (
-  echo Failed to start Johns Print Service v3.
+  echo Failed to start Johns Print Service v6.
   pause
   exit /b 1
 )
