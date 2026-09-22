f1c18cc495155111b5840a575a5c9d11217021e5
Premier <sayed3la2@gmail.com>
2026-09-22 15:59:13 +0300

    chore(print-agent-v8): refresh V8.1 Lite tray label

--- print-agent-v8/TrayHostForm.cs
+++ print-agent-v8/TrayHostForm.cs
@@ -99,7 +99,7 @@ internal sealed class TrayHostForm : Form
         });
 
         _tray.Icon = SystemIcons.Application;
-        _tray.Text = "Smouha Form Print Agent V8";
+        _tray.Text = "Smouha Form Print Agent V8.1 Lite";
         _tray.Visible = true;
         _tray.ContextMenuStrip = menu;
         _tray.DoubleClick += (_, _) => ShowAgent();
