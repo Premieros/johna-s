79956ee631cb460156afb4e862318e45e315f57f
Premier <sayed3la2@gmail.com>
2026-09-24 17:04:28 +0300

    feat: mount work authorization boundary around app routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { AppRoutes } from './routes';
 import { SessionProfileGuard } from '@/core/security/SessionProfileGuard';
 import { CloudPrintAgent } from '@/features/pos/components/settings/CloudPrintAgent';
 import { APP_ROUTES } from '@/core/navigation/routes';
+import { WorkAuthorizationAppBoundary } from '@/features/admin/work-authorization/WorkAuthorizationAppBoundary';
 
 const FinancialVisibilityAdminControl = lazy(() =>
   import('@/features/admin/components/FinancialVisibilityAdminControl').then((module) => ({
@@ -44,7 +45,9 @@ export default function App() {
   return (
     <AppProviders>
       <SessionProfileGuard>
-        <AppRoutes />
+        <WorkAuthorizationAppBoundary>
+          <AppRoutes />
+        </WorkAuthorizationAppBoundary>
         <RouteScopedExtras />
       </SessionProfileGuard>
     </AppProviders>
